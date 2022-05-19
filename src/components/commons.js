374d057d50391227b0d80bc2eda7a216a74622a0
alihassaan993 <31654065+alihassaan993@users.noreply.github.com>
2022-05-19 23:20:19 +0400

    .

--- src/components/commons.js
+++ src/components/commons.js
@@ -1,4 +1,4 @@
-//export const URL="https://60fdsy8ai9.execute-api.us-east-1.amazonaws.com/dev"
+export const URL="https://60fdsy8ai9.execute-api.us-east-1.amazonaws.com/dev"
 export const IMGURL="https://mobimart.s3.ap-south-1.amazonaws.com/"
-export const URL="http://localhost:3000/dev"
+//export const URL="http://localhost:3000/dev"
 export const BUCKET_NAME="mobimart-images"
